1211354a8f5efb6ff263a5a4b0f12e4ce0480209
Sadness <rosalimavictor@gmail.com>
2025-05-13 13:09:17 -0300

    refactor: remove unused import of StatisticsService from app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { StatisticsService } from './statistics/statistics.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StatisticsModule } from './statistics/statistics.module';
 import { SkinsModule } from './skins/skins.module';
